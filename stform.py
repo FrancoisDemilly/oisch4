d8e4521355da3c1661936227c0dcd0a7adbafe63
FrancoisDemilly <francois.demilly@gmail.com>
2023-07-31 16:41:15 +0200

    modif du path v5

--- stform.py
+++ stform.py
@@ -9,7 +9,7 @@ from streamlit_folium import st_folium
 #local path
 #image_name = './DSCN0027.jpg'
 #github path
-image_name= '~/Documents/ied/IED - PARIS8 - L1 INFO/Cursus 2020-2021/Semestre 2/Compétences transversales/OutilsInformatiquesCollaboratifs/oisch4/DSCN0027.jpg'
+image_name= '/home/francois/Documents/ied/IED - PARIS8 - L1 INFO/Cursus 2020-2021/Semestre 2/Compétences transversales/OutilsInformatiquesCollaboratifs/oisch4/DSCN0027.jpg'
 
 #open image with pillow to display it on the screen with streamlit
 img_display = Image.open(image_name)
